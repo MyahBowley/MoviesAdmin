5af98c6d5dd9a00fdcb9b83b630c50d87b52d2eb
Myah <myahbowley@gmail.com>
2026-09-22 15:33:48 -0300

    Added Movie and TV_Show Models.

---
+++ MoviesAdmin/Models/Movie.cs
@@ -0,0 +1,27 @@
+﻿namespace MoviesAdmin.Models
+{
+    public class Movie
+    {
+        public int Id { get; set; }
+
+        // title
+        //synopsis
+        //genre
+        //rating (pg13)
+        //runtime: hours/min (ex: 1h35m
+        //release date
+        // videos
+        // photos
+        // cast & crew (list?)
+            // Director
+            // producer
+            // screenwriter
+            // distributor
+            // Production Co
+            // Original Language
+            // Box Office (money)
+
+
+
+    }
+}
---
+++ MoviesAdmin/Models/TV Show.cs
@@ -0,0 +1,10 @@
+﻿namespace MoviesAdmin.Models
+{
+
+    // I added this myself, as an additional thing.
+
+    public class TV_Show
+    {
+
+    }
+}
